731a02011fdcf52c02caceba1fc93e7e701bb2b9
SkliarovaNastya <116487853+8Anastasiia8@users.noreply.github.com>
2023-04-01 20:34:41 +0300

    First task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+let isActive = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  if (isActive) {
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+  isActive = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  isActive = true;
+});
