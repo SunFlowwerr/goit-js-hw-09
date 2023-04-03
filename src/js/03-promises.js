bc832388ebcf4c217753e1944be7589c50a9ad56
SkliarovaNastya <116487853+8Anastasiia8@users.noreply.github.com>
2023-04-03 14:04:57 +0300

    Homework 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      return resolve({ position, delay });
+    } else {
+      return reject({ position, delay });
+    }
+  });
 }
+
+form.addEventListener('submit', func => {
+  func.preventDefault();
+
+  let delay = +form['delay'].value;
+  const step = +form['step'].value;
+  const amount = +form['amount'].value;
+  let position = 0;
+
+  for (let i = 1; i <= amount; i += 1) {
+    position = 1;
+
+    const firstDelay = delay;
+    let tempDelay = firstDelay + step;
+
+    createPromise(position, firstDelay, tempDelay)
+      .then(({ position, tempDelay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${tempDelay}ms`
+          );
+        }, tempDelay);
+      })
+
+      .catch(({ position, tempDelay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.warning(
+            `❌ Rejected promise ${position} in ${tempDelay}ms`
+          );
+        }, tempDelay);
+      });
+  }
+});
